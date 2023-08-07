27bd298bc42bc7be3e199934a92a8799337adbe0
imedisa <medisayep@gmail.com>
2023-08-07 13:48:50 +0330

    writed number input

--- src/pages/AuthPage.vue
+++ src/pages/AuthPage.vue
@@ -1,6 +1,20 @@
 <template>
-  <div>
-    
+  <div class="h-[calc(100vh-2rem)] flex justify-items-center items-center">
+    <div class="flex flex-col rounded-3xl justify-center align-center mx-5" style="background-color: var(--Background-secondary);">
+        <div class="text-center py-8 px-5 sm:px-14 lg:px-32 lg:py-16">
+            <div class="flex flex-row gap-x-3 items-center">
+            <img src="../assets/images/svgs/Emoji.svg" alt="telephone icon =)">
+            <div class="font-bold text-sm lg:text-xl">
+                Enter you phone numberEnter you phone number
+            </div>
+        </div>
+        <div class="">
+            <form action="">
+                <input style="" class="number__input" type="number" placeholder="+91 9823 324223">
+            </form>
+        </div>
+        </div>
+    </div>
   </div>
 </template>
 
